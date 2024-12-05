3760e5e00491869068884fd5b6ea9ede8c2c5389
Lorena Diaz <lorena.diaz.ttc.021@unilibre.edu.co>
2024-12-05 09:29:02 -0500

    Este es mi parte

---
+++ cabeza.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="icon" href="img/logo.png">
+<link rel="stylesheet" type="text/css" href="CSS/style.css">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet">
+<link rel="stylesheet" href=" https://use.fontawesome.com/releases/v5.2.0/css/all.css"> 
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+
+
+  <title>RentaTüPeli</title>
+</head>
+<body>
+  <div class="cabecera">
+      
+        <p>RentaTüPeli</p>
+        <select name="cuidad">
+  <option value="value1">Seleccione ciudad</option>
+  <option value="value2" selected>Barranquilla</option>
+  <option value="value3">Bogotá</option>
+  <option value="value4">Santa Marta</option>
+</select>
+
+      
+  </div>
+ 
